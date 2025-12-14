b0fe10906dcf836824a85e64f1f2b833223c59f7
Giuliano mario <132069668+Giulianom95@users.noreply.github.com>
2025-12-14 16:19:52 -0300

    incrementa anotacoes do lombock as tarefas reduzindo codigo

--- src/main/java/dev/pessoalprojects/cadastro/Tarefas/TarefasModel.java
+++ src/main/java/dev/pessoalprojects/cadastro/Tarefas/TarefasModel.java
@@ -2,9 +2,15 @@ package dev.pessoalprojects.cadastro.Tarefas;
 
 import dev.pessoalprojects.cadastro.Pessoas.PessoaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "cadastro_tarefas")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class TarefasModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
