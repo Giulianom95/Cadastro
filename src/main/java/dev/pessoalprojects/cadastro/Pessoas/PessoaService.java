22ab18c8d12456c153bd4be2ee27754e01764c8d
Giuliano mario <132069668+Giulianom95@users.noreply.github.com>
2026-01-06 11:30:52 -0300

    fix: injeta o repository e cria a logica das rotas de listar e listar por id, com exceção de listar por id inexistente

--- src/main/java/dev/pessoalprojects/cadastro/Pessoas/PessoaService.java
+++ src/main/java/dev/pessoalprojects/cadastro/Pessoas/PessoaService.java
@@ -1,4 +1,26 @@
 package dev.pessoalprojects.cadastro.Pessoas;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class PessoaService {
+    private PessoaRepository pessoaRepository;
+
+    public PessoaService(PessoaRepository pessoaRepository) {
+        this.pessoaRepository = pessoaRepository;
+    }
+
+    // Listar todas as pessoas
+    public List<PessoaModel> listarPessoas() {
+        return pessoaRepository.findAll();
+    }
+
+    // Listar pessoas por id
+    public PessoaModel listarPessoaPorId(Long id) {
+        Optional<PessoaModel> pessoaPorId = pessoaRepository.findById(id);
+        return pessoaPorId.orElse(null);
+    }
 }
